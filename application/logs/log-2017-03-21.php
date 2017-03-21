02f38c37cc1e6af4e6839d0797b255a9f9e56062
ktracaengemark-AOC <marciorodeng@gmail.com>
2017-03-20 22:22:09 -0300

    primeira carga do sistema

---
+++ application/logs/log-2017-03-21.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-03-21 00:05:16 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:05:55 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:06:20 --> Severity: Notice --> Undefined index: idApp_Tarefa C:\xampp\htdocs\sisautonomo - localhost1\application\controllers\Tarefa.php 203
+ERROR - 2017-03-21 00:07:14 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:07:14 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 333
+ERROR - 2017-03-21 00:07:14 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 334
+ERROR - 2017-03-21 00:07:28 --> Severity: Notice --> Undefined index: auditoriaitem C:\xampp\htdocs\sisautonomo - localhost1\application\controllers\Tarefa.php 570
+ERROR - 2017-03-21 00:07:32 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:07:32 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 333
+ERROR - 2017-03-21 00:07:32 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 334
+ERROR - 2017-03-21 00:07:45 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:07:45 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 333
+ERROR - 2017-03-21 00:07:45 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 334
+ERROR - 2017-03-21 00:08:15 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:08:15 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 333
+ERROR - 2017-03-21 00:08:15 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 334
+ERROR - 2017-03-21 00:08:26 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:08:26 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 333
+ERROR - 2017-03-21 00:08:26 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 334
+ERROR - 2017-03-21 00:09:50 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
+ERROR - 2017-03-21 00:16:29 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo - localhost1\application\views\orcatrata\form_orcatrata.php 729
+ERROR - 2017-03-21 00:16:29 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo - localhost1\application\views\orcatrata\form_orcatrata.php 730
+ERROR - 2017-03-21 00:39:08 --> Severity: Notice --> Undefined index: Profissional C:\xampp\htdocs\sisautonomo - localhost1\application\views\tarefa\form_tarefa.php 330
---
+++ application/views/basico/1nav_principal.php
@@ -0,0 +1,123 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+
+    <div class="row">
+        <div class="col-md-9">
+
+            <ul class="nav navbar-nav navbar-left"> 			
+				<li>												
+					<?php echo form_open(base_url() . 'cliente/pesquisar', 'class="navbar-form navbar-left"'); ?>
+					<form>
+					<div class="input-group">
+						<input type="text" placeholder="Pesquisar Cliente" class="form-control" name="Pesquisa" value="">
+						<span class="input-group-btn">
+							<button class="btn btn-info" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+						</span>
+					</div>						
+					</form>
+					<li><a class="navbar-brand" href="<?php echo base_url(); ?>agenda">AGENDA</a></li>
+				</li>				
+				
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cadastros<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>cliente/pesquisar">Clientes</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>profissional/pesquisar">Prof & Func.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>empresa/pesquisar">Empresas & Fornec.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>funcao/cadastrar/funcao">Função dos Prof. & Func.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>atividade/cadastrar/atividade">Atividade das Empresas & Fornec.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>formapag/cadastrar/formapag">Forma de Pagamento</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>tipodespesa/cadastrar/tipodespesa">Tipo de Despesa</a></li>
+						<li role="separator" class="divider"></li>						
+						<li><a href="<?php echo base_url() ?>relapes/cadastrar">Relação Pessoal</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relacom/cadastrar">Relação Comercial</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>servico/cadastrar">Tabela de Preço de Serviços</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>produto/cadastrar">Tabela de Preço de Produtos</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Anotações<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>cliente/pesquisar">Consultas, Reuniões & Sessões</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>profissional/pesquisar">Bloco de Tarefas</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+				
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Financeiro<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>cliente/pesquisar">Orçamentos / Entradas</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>despesa/cadastrar/despesa">Despesas / Saídas</a></li>
+						<li role="separator" class="divider"></li>
+
+					</ul>
+				</li>
+	
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Relatórios<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="<?php echo base_url() ?>relatorio/clientes">Clientes</a></li>											
+						<li role="separator" class="divider"></li>						
+						<li><a href="<?php echo base_url() ?>relatorio/profissionais">Prof. & Func.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relatorio/empresas">Empresas & Fornec.</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relatorio/orcamentopc">Orçamentos / Procedimentos</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relatorio/orcamento">Orçamentos / Entradas</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relatorio/despesa">Despesas / Saídas</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="<?php echo base_url() ?>relatorio/financeiro">Balanço</a></li>
+						<li role="separator" class="divider"></li>					
+						<li><a href="<?php echo base_url() ?>">Estoque</a></li>
+						<li role="separator" class="divider"></li>
+					</ul>
+				</li>
+																													
+			</ul>	
+
+        </div>
+
+        <div class="col-md-3">
+            <div class="btn-toolbar navbar-form navbar-right" role="toolbar" aria-label="...">
+                <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn active" id="countdowndiv">
+                        <span class="glyphicon glyphicon-hourglass" id="clock"></span>
+                    </button>
+                </div>
+                <div class="btn-group" role="group" aria-label="...">
+                    <button type="button" class="btn btn-info active">
+                        <span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION['log']['Usuario']; ?>
+                    </button>
+                </div>
+                <div class="btn-group" role="group" aria-label="...">
+                    <a href="<?php echo base_url(); ?>login/sair">
+                        <button type="button" class="btn btn-danger">
+                            <span class="glyphicon glyphicon-log-out"></span> Sair
+                        </button>
+                    </a>
+                </div>
+                <div class="btn-group" role="group" aria-label="..."> </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</nav>
+
+<br>
