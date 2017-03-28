8df546e808678cd8cf5e9c63a6985f8bff8434b3
ktracaengemark-AOC <marciorodeng@gmail.com>
2017-03-28 07:38:46 -0300

    Intrudução Botões de Tarefa na página principal

--- application/logs/log-2017-03-28.php
+++ application/logs/log-2017-03-28.php
@@ -3,3 +3,57 @@
 ERROR - 2017-03-28 03:18:24 --> Severity: Notice --> Undefined index: agenda C:\xampp\htdocs\sisautonomo - localhost1\application\controllers\Cliente.php 329
 ERROR - 2017-03-28 03:18:37 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo - localhost1\application\views\orcatrata\form_orcatrata.php 774
 ERROR - 2017-03-28 03:18:37 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo - localhost1\application\views\orcatrata\form_orcatrata.php 775
+ERROR - 2017-03-28 11:56:15 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
+ERROR - 2017-03-28 11:56:15 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
+ERROR - 2017-03-28 11:56:15 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
+ERROR - 2017-03-28 11:56:54 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 47
+ERROR - 2017-03-28 11:56:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 47
+ERROR - 2017-03-28 11:56:54 --> Severity: Notice --> Undefined index: QuitadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 63
+ERROR - 2017-03-28 11:56:54 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 63
+ERROR - 2017-03-28 11:57:01 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 47
+ERROR - 2017-03-28 11:57:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 47
+ERROR - 2017-03-28 11:57:01 --> Severity: Notice --> Undefined index: QuitadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 63
+ERROR - 2017-03-28 11:57:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_despesa.php 63
+ERROR - 2017-03-28 11:58:56 --> Severity: Notice --> Undefined index: ValorVenda C:\xampp\htdocs\sisautonomo\application\views\formapag\pesq_formapag.php 28
+ERROR - 2017-03-28 11:59:23 --> Severity: Notice --> Undefined index: Quantidade C:\xampp\htdocs\sisautonomo\application\views\produto\pesq_produto.php 56
+ERROR - 2017-03-28 11:59:23 --> Severity: Notice --> Undefined index: ValorCompra C:\xampp\htdocs\sisautonomo\application\views\produto\pesq_produto.php 70
+ERROR - 2017-03-28 12:00:00 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 774
+ERROR - 2017-03-28 12:00:00 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 775
+ERROR - 2017-03-28 12:00:33 --> Severity: Notice --> Undefined variable: query C:\xampp\htdocs\sisautonomo\application\views\despesa\pesq_despesa.php 109
+ERROR - 2017-03-28 12:00:48 --> Severity: Notice --> Undefined index: agenda C:\xampp\htdocs\sisautonomo\application\controllers\Cliente.php 329
+ERROR - 2017-03-28 12:01:06 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-28 12:06:08 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-28 12:06:41 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-28 12:10:36 --> Severity: Notice --> Undefined index: idApp_Tarefa C:\xampp\htdocs\sisautonomo\application\controllers\Tarefa.php 316
+ERROR - 2017-03-28 12:10:43 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-28 12:11:10 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 335
+ERROR - 2017-03-28 12:11:10 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 336
+ERROR - 2017-03-28 12:11:45 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-03-28 12:12:31 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 335
+ERROR - 2017-03-28 12:12:31 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 336
+ERROR - 2017-03-28 12:14:55 --> Severity: Notice --> Undefined index: DataFim C:\xampp\htdocs\sisautonomo\application\controllers\Consulta.php 469
+ERROR - 2017-03-28 12:18:05 --> Severity: Notice --> Undefined index: ServicoConcluido C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:05 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:05 --> Severity: Notice --> Undefined index: ConcluidoProcedtarefa C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:18:05 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:18:12 --> Severity: Notice --> Undefined index: ServicoConcluido C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:12 --> Severity: Notice --> Undefined index: ConcluidoProcedtarefa C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:18:12 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:18:48 --> Severity: Notice --> Undefined index: ServicoConcluido C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:48 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:18:48 --> Severity: Notice --> Undefined index: ConcluidoProcedtarefa C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:18:48 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:19:03 --> Severity: Notice --> Undefined index: ServicoConcluido C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:19:03 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:19:03 --> Severity: Notice --> Undefined index: ConcluidoProcedtarefa C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:19:03 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:19:24 --> Severity: Notice --> Undefined index: ServicoConcluido C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:19:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 78
+ERROR - 2017-03-28 12:19:24 --> Severity: Notice --> Undefined index: ConcluidoProcedtarefa C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:19:24 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_tarefa.php 94
+ERROR - 2017-03-28 12:19:45 --> Severity: Notice --> Undefined index: idApp_Procedtarefa C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 335
+ERROR - 2017-03-28 12:19:45 --> Severity: Notice --> Undefined variable: parcelasrec C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 336
+ERROR - 2017-03-28 12:20:54 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
+ERROR - 2017-03-28 12:20:54 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
+ERROR - 2017-03-28 12:20:54 --> Severity: Notice --> Undefined index: AprovadoOrca C:\xampp\htdocs\sisautonomo\application\views\relatorio\tela_clienteprod.php 25
