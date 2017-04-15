4043ea7bafe2a0fcc2e6ffb9a764fbe2774ce57d
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-04-15 19:27:06 -0300

    ajuste nos botões de tarefa

--- application/logs/log-2017-04-15.php
+++ application/logs/log-2017-04-15.php
@@ -1,3 +1,7 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2017-04-15 12:17:09 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-15 23:57:06 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-15 23:57:33 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-15 23:58:35 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 317
+ERROR - 2017-04-15 23:59:25 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 320
---
+++ application/logs/log-2017-04-16.php
@@ -0,0 +1,26 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-16 00:06:46 --> Severity: Notice --> Undefined index: Cliente C:\xampp\htdocs\sisautonomo\application\views\tarefa\form_tarefa.php 320
+ERROR - 2017-04-16 00:07:06 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:08:40 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 17
+ERROR - 2017-04-16 00:08:40 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 17
+ERROR - 2017-04-16 00:08:40 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:10:01 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 16
+ERROR - 2017-04-16 00:10:01 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 16
+ERROR - 2017-04-16 00:10:01 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:11:51 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-16 00:12:09 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:12:09 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:12:09 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:12:35 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:12:35 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:12:35 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:14:15 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-16 00:14:32 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:14:32 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:14:32 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:14:42 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-16 00:14:58 --> Severity: Notice --> Undefined variable: select C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:14:58 --> Severity: Warning --> Invalid argument supplied for foreach() C:\xampp\htdocs\sisautonomo\application\views\tarefa\list_tarefa.php 20
+ERROR - 2017-04-16 00:14:59 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
+ERROR - 2017-04-16 00:16:30 --> Severity: Notice --> Undefined variable: nav_secundario C:\xampp\htdocs\sisautonomo\application\views\tarefa\tela_tarefa.php 6
