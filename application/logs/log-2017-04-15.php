5b858416811ca709cac81279d0cd8fb7d631a88d
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-04-15 07:37:49 -0300

    Atualizações do Rodrigo

---
+++ application/logs/log-2017-04-15.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-15 12:17:09 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
