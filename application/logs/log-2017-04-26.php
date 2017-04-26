20666c437b935faf66d4ee6284591bec1ee3fbbe
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-04-26 13:50:01 -0300

    Ajuste no nome

---
+++ application/logs/log-2017-04-26.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-26 14:48:04 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
+ERROR - 2017-04-26 14:49:30 --> Severity: Warning --> mysqli::real_connect(): MySQL server has gone away C:\xampp\htdocs\sisautonomo\system\database\drivers\mysqli\mysqli_driver.php 133
