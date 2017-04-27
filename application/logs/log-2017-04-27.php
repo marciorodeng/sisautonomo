5c50ba348d3f07da7130efc2a38c007ce142afb5
ktracaengemark-Itau <marciorodeng@gmail.com>
2017-04-27 18:08:30 -0300

    Alteração solicitada pelo cliente
    Incluí " Quitado e Serciço concluído" no relatório financeiro

---
+++ application/logs/log-2017-04-27.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2017-04-27 22:50:17 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 763
+ERROR - 2017-04-27 22:50:17 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 764
+ERROR - 2017-04-27 22:50:18 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-04-27 22:51:20 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 763
+ERROR - 2017-04-27 22:51:20 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 764
+ERROR - 2017-04-27 22:52:11 --> Severity: Notice --> Undefined index: idApp_Procedimento C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 763
+ERROR - 2017-04-27 22:52:11 --> Severity: Notice --> Undefined index: idApp_ParcelasRec C:\xampp\htdocs\sisautonomo\application\views\orcatrata\form_orcatrata.php 764
--- application/models/Relatorio_model.php
+++ application/models/Relatorio_model.php
@@ -44,7 +44,8 @@ class Relatorio_model extends CI_Model {
                 OT.DataOrca,
                 OT.DataEntradaOrca,
                 OT.ValorEntradaOrca,
-
+				OT.QuitadoOrca,
+				OT.ServicoConcluido,
                 PR.ParcelaRecebiveis,
                 PR.DataVencimentoRecebiveis,
                 PR.ValorParcelaRecebiveis,
@@ -90,6 +91,8 @@ class Relatorio_model extends CI_Model {
                 $row->DataPagoRecebiveis = $this->basico->mascara_data($row->DataPagoRecebiveis, 'barras');
 
                 $row->AprovadoOrca = $this->basico->mascara_palavra_completa($row->AprovadoOrca, 'NS');
+				$row->QuitadoOrca = $this->basico->mascara_palavra_completa($row->QuitadoOrca, 'NS');
+				$row->ServicoConcluido = $this->basico->mascara_palavra_completa($row->ServicoConcluido, 'NS');
                 $row->QuitadoRecebiveis = $this->basico->mascara_palavra_completa($row->QuitadoRecebiveis, 'NS');
 
                 #esse trecho pode ser melhorado, serve para somar apenas uma vez
