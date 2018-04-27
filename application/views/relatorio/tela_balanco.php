ca9b1a5a465b7ea2a6f0096a4ac7d59e078b555f
romatiservicos <ktracaengemark@gmail.com>
2018-04-27 11:20:37 -0300

    correção do erro

--- application/views/relatorio/tela_balanco.php
+++ application/views/relatorio/tela_balanco.php
@@ -11,20 +11,24 @@
 
 				<div class="panel panel-primary">
 
-					<div class="panel-heading"><strong><?php echo $titulo; ?> - Ano</strong>
-						
+					<div class="panel-heading"><strong><?php echo $titulo; ?></strong> - Ano						
 						<?php echo form_open('relatorio/balanco', 'role="form"'); ?>
 
-						<button class="btn btn-sm">
-							<input type="text" class="form-control" maxlength="4" placeholder="AAAA"
-								   autofocus name="Ano" value="<?php echo set_value('Ano', $query['Ano']); ?>">
-						</button>
+							<div class="row">
+								<div class="col-md-2">		
+									<div>
+										<input type="text" class="form-control Numero" maxlength="4" placeholder="AAAA"
+											   autofocus name="Ano" value="<?php echo set_value('Ano', $query['Ano']); ?>">
+									</div>
 
-						<button class="btn btn-sm btn-info" name="pesquisar" value="0" type="submit">
-							<span class="glyphicon glyphicon-search"></span> Pesquisar
-						</button>
+									<button class="btn btn-sm btn-info" name="pesquisar" value="0" type="submit">
+										<span class="glyphicon glyphicon-search"></span> Pesquisar
+									</button>
+								</div>
+							</div>
 					
 					</div>
+					
 					<div class="panel-body">
 						
 						</form>
